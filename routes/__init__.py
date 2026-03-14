4c3ec2f78b938e67e11c64c161a4a7cbc6e362c2
root-FURY127 <113788611+root-FURY127@users.noreply.github.com>
2026-03-15 02:39:38 +0530

    Delete routes directory

--- routes/__init__.py
+++
@@ -1,8 +0,0 @@
-# Expose routers
-from . import dashboard_routes
-from . import user_routes
-from . import deletion_routes
-from . import consent_routes
-from . import logs_routes
-from . import pii_routes
-from . import chatbot_routes
